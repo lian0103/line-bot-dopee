0b920db89d0a8cb7ea71dde38981fa2ad0a3141d
Jason <gt07891@greattree.com.tw>
2021-10-06 11:27:01 +0800

    fix

--- herokuApp.js
+++ herokuApp.js
@@ -27,16 +27,16 @@ app.post("/callback", line.middleware(config), (req, res) => {
     });
 });
 
-app.get("/images/:imgName", (req, res) => {
+app.get("/images/:name", (req, res, next) => {
   var options = {
-    root: path.join(__dirname, "public/images"),
+    root: path.join(__dirname, "public"),
     dotfiles: "deny",
     headers: {
       "x-timestamp": Date.now(),
       "x-sent": true,
     },
   };
-  var fileName = req.params.imgName;
+  var fileName = req.params.name;
   res.sendFile(fileName, options, function (err) {
     if (err) {
       next(err);

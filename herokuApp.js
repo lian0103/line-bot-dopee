ae18c8a93434a756782e59eaaaeaf7ef35d1a7e8
Jason <gt07891@greattree.com.tw>
2021-10-25 17:37:07 +0800

    code clean

--- herokuApp.js
+++ herokuApp.js
@@ -2,28 +2,27 @@
 
 const express = require("express");
 const mongoose = require("mongoose");
-
-const imgRoute = require("./routes/imgRoute");
-const msgRoute = require("./routes/msgRoute");
-const lineServiceRoute = require("./routes/lineBotCallback");
-
 const dbUri =
   "mongodb+srv://lien0103:a12345678@chatroom.f2mhj.mongodb.net/line?retryWrites=true&w=majority";
-
 const app = express();
 const port = process.env.PORT || 3005;
 
-
-app.use(imgRoute);
-app.use(lineServiceRoute);
-app.use(msgRoute);
+app.use(require("./routes/imgRoute"));
+app.use(require("./routes/lineBotCallback"));
+app.use(require("./routes/msgRoute"));
+app.get("*", (req, res) => {
+  res.send(`
+    demo call GET  https://line-bot-doope.herokuapp.com/broadcastAll?msg=line可以推播~~
+    demo call GET  https://line-bot-doope.herokuapp.com/queryTodayMsgRecord
+  `);
+});
 
 mongoose
   .connect(dbUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then((result) => {
+  .then(() => {
     console.log("connected to DB");
     app.listen(port, () => {
       console.log(`line-bot-dopee listening on ${port}`);
